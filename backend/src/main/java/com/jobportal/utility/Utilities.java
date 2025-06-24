f228e1ee352817038e32fcd4301f2508df7c3436
DINA18102002 <theenaxz@gmail.com>
2025-06-24 04:47:17 +0000

    upsert utiilities

--- backend/src/main/java/com/jobportal/utility/Utilities.java
+++ backend/src/main/java/com/jobportal/utility/Utilities.java
@@ -25,20 +25,20 @@ public class Utilities {
 	
 	public static Long getNextSequence(String key) throws JobPortalException {
 		Query query = new Query(Criteria.where("_id").is(key));
-		Update update = new Update();
-		update.inc("seq", 1);
-		FindAndModifyOptions options = new FindAndModifyOptions();
-		options.returnNew(true);
+		Update update = new Update().inc("seq", 1);
+
+		// ✅ Correct method name here
+		FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert(true);
+
 		Sequence seq = mongoOperation.findAndModify(query, update, options, Sequence.class);
-		if(seq == null) throw new JobPortalException("Unable to get sequence id for key : "+key);
+		if (seq == null) throw new JobPortalException("Unable to get sequence id for key : " + key);
 		return seq.getSeq();
 	}
 	
 	public static String generateOTP() {
 		StringBuilder otp = new StringBuilder();
 		SecureRandom random = new SecureRandom();
-		for(int i=0; i<6; i++)otp.append(random.nextInt(10));
+		for (int i = 0; i < 6; i++) otp.append(random.nextInt(10));
 		return otp.toString();
 	}
-	
 }
